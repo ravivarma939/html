36431b851b74be6bcf37beb1f8137d94f61c86a3
ravivarma939 <ravivarma1390450@gmail.com>
2024-06-13 22:10:57 +0530

    Js

--- JAVA Script/books/book.js
+++ JAVA Script/books/book.js
@@ -1,31 +1,62 @@
-const anime = [
+const button=document.getElementById('button');
+const posterInput =document.getElementById('poster');
+const titleInput = document.getElementById('title');
+const maindiv = document.getElementById('output');
+const realinput = document.getElementById('real');
+const marvel = [
   {
-      title: "onepiece",
-      hero: "luffy",
+    poster:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEeu-ZO5tLEVLtdY-Xy-ZD7H3zHNnnKCSS4w&s',
+    title :'IRON MAN',
+    real:'Robert Downey Jr.'
   },
   {
-      title: "solo leveling",
-      hero: "sung jun woo",
+    poster:'https://e0.pxfuel.com/wallpapers/56/823/desktop-wallpaper-spiderman-marvel-thumbnail.jpg',
+    title:'SPIDER MAN',
+    real:'Tom Holland'
   },
   {
-      title: "naruto",
-      hero: "naruto",
+   poster:'https://i.pinimg.com/236x/e0/bb/93/e0bb9357113d8fdd65f29ef55f344220.jpg',
+   title: 'BLACK PANTER',
+   real:'Chadwick Aaron Boseman'
+  },
+  {
+    poster:'https://e0.pxfuel.com/wallpapers/24/351/desktop-wallpaper-moon-knight-moonknight-marvel-thumbnail.jpg',
+    title:'moon kinght',
+    real:'Oscar Isaac'
   }
 ];
-
-const maindiv = document.getElementById('main');
-
-function displaydata() {
-  anime.forEach(function (val) {
-      const mydiv = document.createElement('div');
-      mydiv.classList.add('anime'); // corrected 'classlist' to 'classList'
-      const title = document.createElement('h2');
-      title.textContent = val.title;
-      const hero = document.createElement('h2');
-      hero.textContent = val.hero;
-      mydiv.append(title, hero);
-      maindiv.appendChild(mydiv); // corrected appending 'maindiv' to 'mydiv'
-  });
+function createCard(poster, title,real) {
+  const mydiv = document.createElement('div');
+  const anime = document.createElement('div');
+  anime.classList.add('anime');
+  mydiv.classList.add('mydiv');
+  const image = document.createElement('img');
+  image.classList.add('img');
+  image.src = poster;
+  image.alt = title;
+  const titleElement = document.createElement('h2');
+  titleElement.classList.add('txt');
+  titleElement.textContent = title;
+  const realElement =document.createElement('h2');
+  realElement.classList.add('name');
+  realElement.textContent=real;
+  anime.append(image, titleElement,realElement);
+  mydiv.appendChild(anime);
+  maindiv.appendChild(mydiv);
 }
 
-displaydata();
+marvel.forEach(data => createCard(data.poster, data.title,data.real));
+
+button.addEventListener('click', () => {
+  const posterValue = posterInput.value;
+  const titleValue = titleInput.value;
+  const realValue = realinput.value;
+  if (posterValue && titleValue&& realValue) {
+      createCard(posterValue, titleValue,realValue);
+      posterInput.value = "";
+      titleInput.value = "";
+      realValue.value = "";
+  } else {
+      alert('ENTER URL,ENTER CHARCTER NAME,ENTER HERO REAL NAME');
+  }
+});
